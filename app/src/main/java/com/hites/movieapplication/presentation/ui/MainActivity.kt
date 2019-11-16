54dcf4736c578cd7aac2397ebfd82fe7302e73ee
Hitesh Chalise <hiteshchalise10@gmail.com>
2019-11-16 11:17:45 +0545

    movies are now displayed

--- app/src/main/java/com/hites/movieapplication/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/hites/movieapplication/presentation/ui/MainActivity.kt
@@ -2,11 +2,16 @@ package com.hites.movieapplication.presentation.ui
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.hites.movieapplication.R
 import com.hites.movieapplication.core.NetworkHandler
+import com.hites.movieapplication.domain.model.Movie
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 
@@ -18,34 +23,64 @@ class MainActivity : DaggerAppCompatActivity() {
     @Inject
     lateinit var networkHandler: NetworkHandler
 
-    lateinit var mainViewModel: MainViewModel
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var popularRecyclerView: RecyclerView
+    private lateinit var nowPlayingRecyclerView: RecyclerView
+
+    private lateinit var popularViewAdapter: PopularViewAdapter
+    private lateinit var nowPlayingViewAdapter: NowPlayingViewAdapter
+
+    private lateinit var popularViewManager: RecyclerView.LayoutManager
+    private lateinit var nowPlayingViewManager: RecyclerView.LayoutManager
+
+    private var popularMovieList: List<Movie> = ArrayList()
+    private var nowPlayingMovieList: List<Movie> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        popularRecyclerView = findViewById(R.id.recyclerViewPopular)
+        nowPlayingRecyclerView = findViewById(R.id.recyclerViewNowPlaying)
+        popularViewManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        nowPlayingViewManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        popularViewAdapter = PopularViewAdapter(this) {
+            Toast.makeText(this, "Item Clicked: $it", Toast.LENGTH_SHORT).show()
+        }
+        nowPlayingViewAdapter = NowPlayingViewAdapter(this) {
+            Toast.makeText(this, "Item Clicked: $it", Toast.LENGTH_SHORT).show()
+        }
+        popularRecyclerView.layoutManager = popularViewManager
+        popularRecyclerView.adapter = popularViewAdapter
+        nowPlayingRecyclerView.layoutManager = nowPlayingViewManager
+        nowPlayingRecyclerView.adapter = nowPlayingViewAdapter
+
         initializeViewModel()
         loadPage()
     }
 
-    private fun initializeViewModel(){
+    private fun initializeViewModel() {
         mainViewModel = ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java)
         mainViewModel.liveDataNowPlayingList.observe(this, Observer {
             Log.d("MovieApplication: ", "OnCreate: ${it.size}")
+            nowPlayingMovieList = it
+            nowPlayingViewAdapter.submitList(nowPlayingMovieList)
         })
-        mainViewModel.failureNowPlayingLiveData.observe(this, Observer{
+        mainViewModel.failureNowPlayingLiveData.observe(this, Observer {
             Log.e("MovieApplication: ", "Failed: {$it}")
-        } )
+        })
 
         mainViewModel.liveDataPopularMovieList.observe(this, Observer {
             Log.d("MovieApplication: ", "OnCreate: ${it.size}")
+            popularMovieList = it
+            popularViewAdapter.submitList(popularMovieList)
         })
-        mainViewModel.failureGetPopularLiveData.observe(this, Observer{
+        mainViewModel.failureGetPopularLiveData.observe(this, Observer {
             Log.e("MovieApplication: ", "Failed: {$it}")
-        } )
+        })
     }
 
-    private fun loadPage(){
+    private fun loadPage() {
         val networkAvailability: Boolean = networkHandler.isConnected
         Log.d("MovieApplication", "MainActivity: $networkAvailability")
         mainViewModel.loadNowPlayingList(!networkAvailability)
---
+++ app/src/main/java/com/hites/movieapplication/presentation/ui/NowPlayingViewAdapter.kt
@@ -0,0 +1,71 @@
+package com.hites.movieapplication.presentation.ui
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import coil.api.load
+import coil.transform.CircleCropTransformation
+import coil.transform.RoundedCornersTransformation
+import com.hites.movieapplication.R
+import com.hites.movieapplication.domain.model.Movie
+
+class NowPlayingViewAdapter(private val ctx: Context, private val listener: (Int) -> Unit) :
+    ListAdapter<Movie, NowPlayingViewAdapter.MyViewHolder>(DIFF_CALLBACK) {
+
+    class MyViewHolder(itemView: View, listener: (Int) -> Unit) :
+        RecyclerView.ViewHolder(itemView) {
+        var title: TextView = itemView.findViewById(R.id.item_title)
+        var imageView: ImageView = itemView.findViewById(R.id.item_image)
+
+        init {
+            itemView.setOnClickListener {
+
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    listener(position)
+                }
+
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): MyViewHolder {
+        val v =
+            LayoutInflater.from(viewGroup.context).inflate(R.layout.item_layout, viewGroup, false)
+        return MyViewHolder(v, listener)
+    }
+
+    override fun onBindViewHolder(myViewHolder: MyViewHolder, i: Int) {
+        val tempTitle = getItem(i).title
+        myViewHolder.title.text = tempTitle
+        myViewHolder.imageView.load("https://image.tmdb.org/t/p/w500/" + getItem(i).poster_path) {
+            crossfade(true)
+            placeholder(CircularProgressDrawable(ctx))
+            transformations(RoundedCornersTransformation(12.0f))
+        }
+    }
+
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Movie>() {
+            override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+                return oldItem.title == newItem.title &&
+                        oldItem.poster_path == newItem.poster_path &&
+                        oldItem.adult == newItem.adult &&
+                        oldItem.vote_average == newItem.vote_average
+            }
+        }
+    }
+
+}
